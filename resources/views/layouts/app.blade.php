712e1b84ca7ebd1709f776cb514afa57201065a4
fahmimajd <fahmifhm@gmail.com>
2026-02-13 04:04:06 +0000

    tampilan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,7 @@
                 </a>
                 @endif
 
-                @if(!auth()->user()->isPetugas())
+                {{-- Permit Kinerja for Petugas as well --}}
                 <a href="{{ route('kinerja.index') }}" class="sidebar-link {{ request()->routeIs('kinerja.*') ? 'active' : '' }}">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 00-2-2m0 0h2a2 2 0 012 2v6a2 2 0 002 2h2a2 2 0 002-2v-6a2 2 0 00-2-2h-2a2 2 0 00-2 2v6a2 2 0 002 2z"></path>
@@ -86,9 +86,9 @@
                     </span>
                     @endif
                 </a>
-                @endif
 
-                @if(!auth()->user()->isPendamping())
+                {{-- Hide Kinerja Kecamatan for Pendamping AND Petugas Desa (who have kode_desa) --}}
+                @if(!auth()->user()->isPendamping() && !(auth()->user()->isPetugas() && auth()->user()->kode_desa))
                 <a href="{{ route('kinerja-kecamatan.index') }}" class="sidebar-link {{ request()->routeIs('kinerja-kecamatan.*') ? 'active' : '' }}">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
